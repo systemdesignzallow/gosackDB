2efd1e5f31478405272afab13cf1d2140687eacf
Patrick Gosack <gosack@gmail.com>
2019-08-02 21:21:21 -0700

    working for ec2

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,5 @@ git clone https://github.com/systemdesignzallow/gosackDB
 cd gosackDB
 sudo docker pull mariadb/server:10.3
 sudo docker build --build-arg MARIADB_ROOT_PASSWORD=mypassword -t gosackdb .
-sudo docker run -d --name newdb -eMARIADB_ROOT_PASSWORD=mypassword gosackdb
-sudo docker exec -it newdb curl https://gosackdata.s3-us-west-1.amazonaws.com/small.csv -o /var/lib/mysql/data.csv
-sudo docker exec -it newdb mysql -u root -p$MARIADB_ROOT_PASSWORD < bootstrap.sql
-
+sudo docker run -d --name newdb -p 3306:3306 -eMARIADB_ROOT_PASSWORD=mypassword gosackdb
 
