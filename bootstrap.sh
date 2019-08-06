9c48223c0298b5ed86afef48526a2caa6a9874f7
Patrick Gosack <gosack@gmail.com>
2019-08-05 22:46:23 -0700

    add external data volume mount

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,12 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo yum install git -y
 cd ~
+
 git clone https://github.com/systemdesignzallow/gosackDB
 cd gosackDB
+mkdir data
+sudo mount /dev/xvdb data
+
 sudo docker pull mariadb/server:10.3
 sudo docker build --build-arg MARIADB_ROOT_PASSWORD=mypassword -t gosackdb .
 sudo docker run -d --name newdb -p 3306:3306 -eMARIADB_ROOT_PASSWORD=mypassword gosackdb
-
